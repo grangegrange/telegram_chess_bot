3fbfd275b76fed129dda657c1e838bed61af605b
igor grange <igorgrange@gmail.com>
2020-04-23 14:26:52 +0200

    adding commands & bot start/stop game cases

---
+++ constants/commands.js
@@ -0,0 +1,5 @@
+module.exports = Object.freeze({
+    BOT_START: "/start",
+    CHESS_START: "Играем",
+    CHESS_STOP: "Закончить игру"
+});
--- index.js
+++ index.js
@@ -5,17 +5,62 @@ const token = process.env.TOKEN
 const bot = new TelegramBot(token, { polling: true })
 
 
+// CONSTANTS & FUNCTIONS
+const COMMANDS = require('./constants/commands')
+
+// STATE
+let chessPlaying = false
+
 bot.on('message', (msg) => {
 
-    if (msg.text === '/start') {
-        bot.sendMessage(msg.chat.id, "Шахматы?", {
+    if (!chessPlaying && Object.values(COMMANDS).indexOf(msg.text) !== -1) {
+
+        if (msg.text === COMMANDS.BOT_START) {
+            bot.sendMessage(msg.chat.id, "Шахматы?", {
+                "reply_markup": {
+                    "keyboard": [
+                        ["Играем"]
+                    ]
+                }
+            });
+        }
+
+
+        if (msg.text === COMMANDS.CHESS_START) {
+
+            chessPlaying = true;
+
+            bot.sendMessage(msg.chat.id, "Ну-ка посмотрим", {
+                "reply_markup": {
+                    "keyboard": [
+                        ["Закончить игру"]
+                    ]
+                }
+            })
+
+            bot.sendMessage(msg.chat.id, "Игра началась!")
+
+        }
+
+    }
+    else if (chessPlaying && msg.text === COMMANDS.CHESS_STOP) {
+
+        chessPlaying = false;
+
+        bot.sendMessage(msg.chat.id, "Игра закончена. Еще?", {
             "reply_markup": {
                 "keyboard": [
-                    ["Шахматы"]
+                    ["Играем"]
                 ]
             }
         });
+
     }
+    else {
+
+    }
+
+
 
 });
 
