f37d92ab9f5e35a26abe241b1c406937170c7657
igor grange <igorgrange@gmail.com>
2020-04-27 12:19:41 +0200

    adding board drawing functions & pieces images

---
+++ functions/board.js
@@ -0,0 +1,49 @@
+const { Image, createCanvas } = require('canvas')
+
+const drawImageCanvas = (gameBoard) => {
+
+    const canvas = createCanvas(400, 400)
+    const context = canvas.getContext('2d')
+    const img = new Image()
+
+    const squareWidth = 50
+    const totalSquares = 64
+    const boardSize = 8
+    const colorLigth = '#EAD8C3'
+    const colorDark = '#B08368'
+
+    let i, x, y = -1
+
+    for (i = 0; i < totalSquares; i++) {
+        x++
+        if (i % boardSize == 0) {
+            y++
+            x = 0
+        }
+
+        context.beginPath()
+        context.rect(x * squareWidth, y * squareWidth, squareWidth, squareWidth)
+        context.fillStyle = (x + y) % 2 ? colorDark : colorLigth
+        context.fill()
+
+        const piece = gameBoard[y][x]
+        if (piece !== null) {
+            img.src = `./imgs/${piece.color}${piece.type.toUpperCase()}.svg`
+            context.drawImage(img, x * squareWidth, y * squareWidth, 50, 50)
+        }
+
+    }
+
+    const boardBuffer = Buffer.from(canvas.toDataURL().slice(22), "base64")
+    return boardBuffer
+
+}
+
+
+const reverseArray = (array) => {
+    return array.slice().reverse()
+}
+
+
+module.exports.drawImageCanvas = drawImageCanvas
+module.exports.reverseArray = reverseArray
--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 const TelegramBot = require('node-telegram-bot-api')
 require('dotenv').config()
 const token = process.env.TOKEN
+const { Chess } = require('chess.js')
 
 const bot = new TelegramBot(token, { polling: true })
 
 
 // CONSTANTS & FUNCTIONS
 const COMMANDS = require('./constants/commands')
+const boardFunctions = require('./functions/board')
 
 // STATE
 let chessPlaying = false
+let chessGame = {}
 
 bot.on('message', (msg) => {
 
+    // NOT PLAYING & IN COMMANDS LIST
     if (!chessPlaying && Object.values(COMMANDS).indexOf(msg.text) !== -1) {
 
         if (msg.text === COMMANDS.BOT_START) {
@@ -22,14 +26,12 @@ bot.on('message', (msg) => {
                         ["Играем"]
                     ]
                 }
-            });
+            })
         }
 
-
         if (msg.text === COMMANDS.CHESS_START) {
-
-            chessPlaying = true;
-
+            chessPlaying = true
+            chessGame = new Chess()
             bot.sendMessage(msg.chat.id, "Ну-ка посмотрим", {
                 "reply_markup": {
                     "keyboard": [
@@ -37,30 +39,30 @@ bot.on('message', (msg) => {
                     ]
                 }
             })
-
-            bot.sendMessage(msg.chat.id, "Игра началась!")
-
+            bot.sendMessage(msg.chat.id, "Начинаем игру!")
+            const boardImage = boardFunctions.drawImageCanvas(chessGame.board())
+            bot.sendPhoto(msg.chat.id, boardImage)
         }
 
     }
+
+    // STOP PLAYING
     else if (chessPlaying && msg.text === COMMANDS.CHESS_STOP) {
 
         chessPlaying = false;
-
         bot.sendMessage(msg.chat.id, "Игра закончена. Еще?", {
             "reply_markup": {
                 "keyboard": [
                     ["Играем"]
                 ]
             }
-        });
+        })
 
     }
+
+    // NOT A COMMAND
     else {
 
     }
 
-
-
-});
-
+})
